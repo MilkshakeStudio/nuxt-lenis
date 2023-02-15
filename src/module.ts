9739b0422eedc26f6100124c0c97be35b18ae2d2
Simon Goetz <simon@milkshake.studio>
2023-02-15 14:02:32 -0500

    add imports

--- src/module.ts
+++ src/module.ts
@@ -4,6 +4,7 @@ import {
    defineNuxtModule,
    addPlugin,
    createResolver,
+   addImports,
 } from "@nuxt/kit";
 
 export interface ModuleOptions {
@@ -14,15 +15,29 @@ export default defineNuxtModule<ModuleOptions>({
    meta: {
       name: "@nuxt-lenis",
       configKey: "nuxtLenis",
+      compatibility: {
+         nuxt: "^3.0.0",
+      },
    },
    defaults: {
       addPlugin: true,
    },
-   setup(options, nuxt) {
+   async setup(options, nuxt) {
       //  if (options.addPlugin) { }
       const { resolve } = createResolver(import.meta.url);
       const runtimeDir = fileURLToPath(new URL("./runtime", import.meta.url));
       nuxt.options.build.transpile.push(runtimeDir);
+
+    
+      addImports([
+         {
+            name: "default",
+            as: "Lenis",
+            from: "@studio-freight/lenis",
+         },
+         // { name: 'useScrollState', as: 'useScrollState', from: join(runtimeDir, 'composables') },
+      ]);
+
       addPlugin(resolve(runtimeDir, "plugin"));
       addComponent({
          name: "lenis", // name of the component to be used in vue templates
--- src/runtime/plugin.ts
+++ src/runtime/plugin.ts
@@ -1,10 +1,11 @@
 import { defineNuxtPlugin } from "#app";
-import Lenis from "@studio-freight/lenis";
+// import Lenis from "@studio-freight/lenis";
 import { useScrollState } from './composables/scrollState'
+import { Lenis } from '#imports'
 
 export default defineNuxtPlugin((nuxtApp) => {
    const [scrollState, setScrollState] = useScrollState();
-   
+      
    nuxtApp.vueApp.provide("Lenis", Lenis);
    nuxtApp.vueApp.provide("setScrollState", setScrollState);
    nuxtApp.vueApp.provide("scrollState", scrollState);
